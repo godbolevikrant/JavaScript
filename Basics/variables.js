d5707fd03a22c75d51eeafab83c3505b52a4f952
Godbole Vikrant <58286651+godbolevikrant@users.noreply.github.com>
2024-09-26 07:24:14 +0000

    Update

---
+++ Basics/variables.js
@@ -0,0 +1,10 @@
+const accountId=100;
+let accountEmail="godbole.vikrant";
+var accountPassword="Vikcy@123";
+accountCity="Pune"
+
+// accountId="2"; not allowed
+
+// accountEmail="godbole.vikrant1" allowed
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
