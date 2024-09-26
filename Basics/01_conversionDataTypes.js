1b362d78414b2d9264351ae5791c6eb40ce2e5bc
Godbole Vikrant <58286651+godbolevikrant@users.noreply.github.com>
2024-09-26 07:52:02 +0000

    update

---
+++ Basics/01_conversionDataTypes.js
@@ -0,0 +1,10 @@
+let num="100";
+console.log(typeof(num));
+
+let valueNum=Number(num)
+console.log(typeof(valueNum));
+
+
+let isLoggedIn=0; 
+let booleanLoggedIn=Boolean(isLoggedIn);
+console.log(booleanLoggedIn);
---
+++ Basics/dataTypes.js
@@ -0,0 +1,9 @@
+let name1="Vikrant Godbole"; 
+let num=23;                 //int or number
+let isLoggedIn=true;        //boolean 
+let state ;                // undefined
+let etc=null;              // null
+
+
+console.table([name1,num,isLoggedIn,state,etc]);
+console.log(typeof"name1");
