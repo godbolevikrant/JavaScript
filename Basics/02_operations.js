e7ff4cd2bda6343e9205ed875aa0ef59fae8307c
Godbole Vikrant <58286651+godbolevikrant@users.noreply.github.com>
2024-09-26 08:07:18 +0000

    update

---
+++ Basics/02_operations.js
@@ -0,0 +1,15 @@
+let value=3;
+let negValue=-value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(5-2);
+console.log(5*5);
+console.log(50/10);
+console.log(2**2);
+
+let str1="Vikrant";
+let str2=" Godbole";
+
+str3=str1+str2;
+console.log(str3);
